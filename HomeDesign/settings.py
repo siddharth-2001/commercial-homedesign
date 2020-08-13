7c020ae12c297d405019c68d4ef4a05717990616
Siddharth <siddharthbharmoria@gmail.com>
2020-08-14 02:20:33 +0530

    now optimised for mobile

--- HomeDesign/settings.py
+++ HomeDesign/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages',
     'phone_field',
+    'appointments',
 ]
 
 MIDDLEWARE = [
--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Appointment
+from django.http import HttpResponse
+from .models import Appointment
 
 # Create your views here.
+
+def book_appointment(request):
+    name = request.POST.get('name')
+    phone = request.POST.get('phone')
+    return render(request,'appointment.html')
+
